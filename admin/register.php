a4b9b5ac9ac38c61a0b558bad0191ea2e9a405f0
khoa huynh <khoa.hn3005@gmail.com>
2020-01-01 23:23:53 +0700

    FETCH DATA FROM DATABASE

--- admin/register.php
+++ admin/register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include('includes/header.php');
 include('includes/navbar.php');
 ?>
@@ -10,61 +11,131 @@ include('includes/navbar.php');
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="addadminprofile">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Add Admin Data</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+      <form action="code.php" method="POST">
+        <div class="modal-body">
+
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control" placeholder="Enter Username">
+          </div>
+
+          <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" class="form-control" placeholder="Enter email">
+          </div>
+
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" placeholder="Enter email">
+          </div>
+
+          <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" name="confirmpassword" class="form-control" placeholder="Enter email">
+          </div>
+
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" name="registerbtn" class="btn btn-primary">Save</button>
+        </div>
+      </form>
+
     </div>
   </div>
 </div>
 
-<div class="container-fluid"></div>
+<div class="container-fluid">
 
-<!-- DataTables Example-->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Admin Profile
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">Add Admin Profile</button>
-    </h6>
-  </div>
-</div>
-
-<div class="card-body">
-  <div class="table-responsive">
-    <div class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Position</th>
-          <th>Office</th>
-          <th>Age</th>
-          <th>Start date</th>
-          <th>Salary</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
+  <!-- DataTables Example-->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Admin Profile
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">Add Admin Profile</button>
+      </h6>
     </div>
-  </div>
-</div>
+
+    <div class="card-body">
 
 <?php
-include('includes/footer.php');
+if(isset($_SESSION['success']) && $_SESSION['success'] !='')
+{
+  echo '<h2> '.$_SESSION['success'].' </h2>';
+  unset($_SESSION['success']);
+}
+?>
+
+<?php
+if(isset($_SESSION['status']) && $_SESSION['status'] !='')
+{
+  echo '<h2 class="bg-info"> '.$_SESSION['status'].' </h2>';
+  unset($_SESSION['status']);
+}
+?>
+
+      <div class="table-responsive">
+
+<?php
+$connection = mysqli_connect("localhost","root","","adminpanel");
+$query = "SELECT * FROM register";
+$query_run = mysqli_query($connection, $query);
+?>
+
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th>EDIT</th>
+              <th>DELETE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            if(mysqli_num_rows($query_run)>0)
+            {
+              while($row = mysqli_fetch_assoc($query_run))
+              {
+               ?>
+
+    
+            <tr>
+              <td><?php echo $row['id']; ?></td>
+              <td><?php echo $row['username']; ?></td>
+              <td><?php echo $row['email']; ?></td>
+              <td><?php echo $row['password']; ?></td>
+            <td>
+              <button type="submit" class="btn btn-success">EDIT</button>
+            </td>
+            <td>
+              <button type="submit" class="btn btn-danger">DELETE</button>
+            </td>
+            </tr>
+              <?php
+              }
+            }
+            else{
+              echo "No Record Found";
+            }
+              ?>  
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+<!-- /.container-fluid -->
+<?php
 include('includes/scripts.php');
+include('includes/footer.php');
 ?>
