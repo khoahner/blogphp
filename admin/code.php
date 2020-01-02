0e89c932de20bc21831b491b5d164e14424783cd
khoahner <khoa.hn3092@gmail.com>
2020-01-02 17:00:13 +0700

    UPDATE DATA INTO DATABASE

--- admin/code.php
+++ admin/code.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $connection = mysqli_connect("localhost", "root", "", "adminpanel");
+
 if (isset($_POST['registerbtn'])) {
     $username = $_POST['username'];
     $email = $_POST['email'];
@@ -25,6 +26,28 @@ if (isset($_POST['registerbtn'])) {
 }
 
 
+
+if(isset($_POST['updatebtn']))
+{
+    $id=$_POST['edit_id'];
+    $username = $_POST['edit_username'];
+    $email = $_POST['edit_email'];
+    $password = $_POST['edit_password'];
+
+    $query = "UPDATE register SET username='$username',email='$email',password='$password' WHERE id='$id' ";
+    $query_run = mysqli_query ($connection, $query);
+
+    if($query_run)
+    {
+        $_SESSION['success'] = "Your Data is Updated";
+        header('location: register.php');
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Data is Not Updated";
+        header('location: register.php');
+    }
+}
 
 
 
--- admin/register_edit.php
+++ admin/register_edit.php
@@ -13,39 +13,39 @@ include('includes/navbar.php');
         </div>
         <div class="card-body">
             <?php
-            $connection = mysqli_connect("localhost","root","","adminpanel");
-            if(isset($_POST['edit_btn']))
-            {
+            $connection = mysqli_connect("localhost", "root", "", "adminpanel");
+            if (isset($_POST['edit_btn'])) {
                 $id = $_POST['edit_id'];
                 $query = "SELECT * FROM register WHERE id='$id' ";
-                $query_run = mysqli_query($connection,$query);
+                $query_run = mysqli_query($connection, $query);
 
-                foreach($query_run as $row)
-                {
-                    ?>
+                foreach ($query_run as $row) {
+            ?>
 
-           
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" value="<?php echo $row['username'] ?>" class="form-control" placeholder="Enter Username">
-            </div>
+                    <form action="code.php" method="post">
+                        <input type="hidden" name="edit_id" value="<?php echo $row['id'] ?>">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="edit_username" value="<?php echo $row['username'] ?>" class="form-control" placeholder="Enter Username">
+                        </div>
 
-            <div class="form-group">
-                <label>Email</label>
-                <input type="email" name="email" value="<?php echo $row['email'] ?>" class="form-control" placeholder="Enter email">
-            </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" name="edit_email" value="<?php echo $row['email'] ?>" class="form-control" placeholder="Enter email">
+                        </div>
 
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" value="<?php echo $row['password'] ?>" class="form-control" placeholder="Enter email">
-            </div>
-            <a href="register.php" class="btn btn-danger">Cancel</a>
-            <button class="btn btn-primary">Update</button>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="edit_password" value="<?php echo $row['password'] ?>" class="form-control" placeholder="Enter password">
+                        </div>
+                        <a href="register.php" class="btn btn-danger">Cancel</a>
+                        <button type="submit" name="updatebtn" class="btn btn-primary">Update</button>
+                    </form>
             <?php
+                }
             }
-        }
-        ?>
-            
+            ?>
+
         </div>
     </div>
 </div>
